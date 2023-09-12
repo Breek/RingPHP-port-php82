3989ebc629207d560eed9487521148c63bdda057
Jérémy Chatard <jchatard@breek.fr>
2023-09-12 17:22:26 +0200

    Add return types.

--- src/Future/FutureArray.php
+++ src/Future/FutureArray.php
@@ -8,31 +8,34 @@ class FutureArray implements FutureArrayInterface
 {
     use MagicFutureTrait;
 
-    public function offsetExists($offset)
+
+    public function offsetExists($offset): bool
     {
         return isset($this->_value[$offset]);
     }
 
-    public function offsetGet($offset)
+
+    public function offsetGet($offset): mixed
     {
         return $this->_value[$offset];
     }
 
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         $this->_value[$offset] = $value;
     }
 
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         unset($this->_value[$offset]);
     }
 
-    public function count()
+    public function count(): int
     {
         return count($this->_value);
     }
 
+    #[\ReturnTypeWillChange]
     public function getIterator()
     {
         return new \ArrayIterator($this->_value);
